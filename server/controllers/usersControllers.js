125619f026e4e488b444d7131f21b3b3049e23da
juan <ortiz41mav@gmail.com>
2024-01-17 19:17:31 +0100

    login version 4

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -32,21 +32,20 @@ class usersControllers {
     });
   };
 
-  loginUser = (req, res) => {
-    const { email, password } = req.body;
-    let sql = `SELECT * FROM user WHERE email= "${email}"`;
-    connection.query(sql, (error, result) => {
-      if (error) return res.status(500).json(error);
+  loginUser = (req, res) => { 
+    const {email,password} = req.body;
+    let sql = `SELECT * FROM user WHERE email= "${email}"`
+    connection.query(sql,(error,result)=>{
+      if(error) return res.status(500).json(error)
       console.log(result);
-      if (!result || result.length === 0 || result[0].is_deleted == 1) {
-        res.status(401).json("Usuario no autorizado");
-      } else {
-        const user = result[0];
-        const hash = user.password;
+    if(!result || result.length === 0 || result[0].is_deleted == 1){
+      res.status(401).json("Usuario no autorizado")
+    }else{
+      const user = result[0];
+      const hash = user.password;
 
-        bcrypt.compare(password, hash, (error, response) => {
-          if (error) return res.status(500).json(error);
-
+      bcrypt.compare(password,hash, (error,response)=>{
+        if(error) return res.status(500).json(error)
 
         if(response == true){
           const token = jwt.sign(
@@ -72,28 +71,6 @@ class usersControllers {
     console.log("hola usuario",user_id);
   }
 
-          if (response == true) {
-            const token = jwt.sign(
-              {
-                user: {
-                  user: user.user_id,
-                  type: user.type,
-                },
-              },
-              process.env.SECRET,
-              { expiresIn: "1d" }
-            ); //consultar cuanto tiempo queremos que se guarde la contraseña
-
-
-            res.status(200).json({ token, user });
-          } else {
-            res.status(401).json("Email o contraseña incorrecta");
-          }
-          console.log("responseeeeeeeeeee", response); //con esto probamos si la contraseña coincide(true/false)
-        });
-      }
-    });
-  };
 }
 
 module.exports = new usersControllers();
