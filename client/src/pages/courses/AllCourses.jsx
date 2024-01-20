1606ad11d773b31d8f5ee7dc966630d0431ab7e1
enrique <quiquesaba@gmail.com>
2024-01-20 12:27:24 +0100

    desatasque

--- client/src/pages/courses/AllCourses.jsx
+++ client/src/pages/courses/AllCourses.jsx
@@ -1,57 +1,61 @@
 import React, { useContext, useEffect, useState } from 'react';
 import './courses.scss';
-import axios from 'axios'
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Button, Card } from 'react-bootstrap';
 import { AscendioContext } from '../../context/AscendioContext';
-
 export const AllCourses = () => {
-
-  const {user} = useContext(AscendioContext);
-
-  const [allcourses, setAllcourses] = useState()
-  const [comprado, setComprado] = useState(false)
-  const navigate = useNavigate()
-
-  useEffect((id)=>{
+  const { user } = useContext(AscendioContext);
+  const [allcourses, setAllcourses] = useState([]);
+  const [guardado, setGuardado] = useState({});
+  const navigate = useNavigate();
+  
+  useEffect(() => {
     axios
       .get(`http://localhost:3000/courses/callcourses`)
-      .then((res)=>{
+      .then((res) => {
         console.log(res.data);
-        setAllcourses(res.data)
+        setAllcourses(res.data);
       })
-      .catch((err)=>{
-        console.log(err)})
-  }, [])
-
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const guardar = (courseId) => {
+    setGuardado((prevGuardado) => ({
+      ...prevGuardado,
+      [courseId]: !prevGuardado[courseId],
+    }));
+  };
   return (
     <div>
       <h1>ALL courses</h1>
       <p> TODO: mapeo y Use Effect -array de dependencia con comprado- con todos los cursos</p>
-
-      {allcourses?.map ((elem) =>{
-        return(
-          <Card style={{ width: '18rem' }} key={elem.course_id}>
-          <Card.Img variant="top" src={`http://localhost:3000/images/cursos/${elem.img}`} />
-          <Card.Body>
-            <Card.Title> {elem.title} </Card.Title>
-            <Card.Subtitle>
-              {elem.tags}
-            </Card.Subtitle>
-            <Card.Text>
-              {elem.description}
-            </Card.Text>
-            <Card.Text>
-              {elem.price}€
-            </Card.Text>
-          
-            {/* {user.user_id === res.data.user_id && <Button variant="primary">Eres el creador</Button>} */}
-            <Button onClick={()=>comprarCurso(elem.course_id)} variant="primary">Comprar</Button>
-            <Button variant="primary">Guardar</Button>
-          </Card.Body>
-        </Card>
-        )
+      {allcourses?.map((elem) => {
+        return (
+          <Card style={{ width: '22rem' }} key={elem.course_id}>
+            <Card.Img variant="top" src={`http://localhost:3000/images/cursos/${elem.img}`} />
+            <Card.Body>
+              <Card.Title> {elem.title} </Card.Title>
+              <Card.Subtitle>{elem.tags}</Card.Subtitle>
+              <Card.Text>{elem.description}</Card.Text>
+              <Card.Text>{elem.price}€</Card.Text>
+              <Button onClick={() => comprarCurso(elem.course_id)} variant="outline-success" className="me-3">
+                Comprar
+              </Button>
+              {guardado[elem.course_id] ? (
+                <Button onClick={() => guardar(elem.course_id)} variant="outline-success" className="me-3">
+                  Quitar de favoritos
+                </Button>
+              ) : (
+                <Button onClick={() => guardar(elem.course_id)} variant="outline-success" className="me-3">
+                  Guardar entre favoritos
+                </Button>
+              )}
+            </Card.Body>
+          </Card>
+        );
       })}
     </div>
-  )
-}
+  );
+};
--- server/controllers/coursesControllers.js
+++ server/controllers/coursesControllers.js
@@ -5,7 +5,7 @@ class coursesControllers {
   createCourse = (req, res) => {
     const tags = JSON.parse(req.body.tags)
     const { title, description, price, user_id } = JSON.parse(req.body.crearCurso);
-
+    console.log(tags)
     let img=""
     if(req.file){
        img = req.file.filename;
@@ -18,14 +18,14 @@ class coursesControllers {
       err && res.status(500).json(err)
       if(err){console.log(err)}
       let course_id = result.insertId
-      /* tags.forEach((elem) =>{
+      tags.forEach((elem) =>{
         let sql2 = `INSERT INTO course_tag (course_id, tag_id) VALUES (${course_id}, ${elem.value})`
         connection.query(sql2, (errtag, restag)=>{
           errtag && res.status(500).json(errtag)
           if(errtag){console.log(errtag)}
           
         })
-      }) */
+      })
       res.status(200).json(result)
     })
   }
