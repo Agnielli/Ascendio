07da251271ac2ec17a02031ccd162713a35a237a
julia <juliab.martin@hotmail.com>
2024-01-23 08:02:35 +0100

    'l'

--- client/src/components/FormAddTopic/FormAddTopic.jsx
+++ client/src/components/FormAddTopic/FormAddTopic.jsx
@@ -1,33 +1,26 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { Button, Col, Form, Row } from 'react-bootstrap'
-
-export const FormAddTopic = ({setShowTopic, course_id, sections}) => {
-
-  const [newTopic, setNewTopic] = useState("")
-
-  const handleChange = (e) =>{
-    setNewTopic(e.target.value)
-  }
-
-  let section_id = sections[0].section_id
-
-  const handleSubmit = () =>{
-    let data = {course_id, section_id, newTopic}
-    console.log("data", data)
-
-    if(newTopic !== ''){
+import axios from "axios";
+import React, { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+export const FormAddTopic = ({ setShowTopic, course_id, sections }) => {
+  const [newTopic, setNewTopic] = useState("");
+  const handleChange = (e) => {
+    setNewTopic(e.target.value);
+  };
+  let section_id = sections[0].section_id;
+  const handleSubmit = () => {
+    let data = { course_id, section_id, newTopic };
+    console.log("data", data);
+    if (newTopic !== "") {
       axios
         .post("http://localhost:3000/courses/addtopic", data)
-        .then((res)=>{
-          console.log(res)
+        .then((res) => {
+          console.log(res);
         })
-        .catch((err)=>{
-          console.log(err)
-        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-  }
-  //Tengo que recibir el coourse_id y el section_id para que un botón que está aquí provoque un axios que guarde en bd en la tabla topic. Aquí está el input tipo file.
+  };
   return (
     <Row>
       <Col>
@@ -40,13 +33,23 @@ export const FormAddTopic = ({setShowTopic, course_id, sections}) => {
               value={newTopic}
               onChange={handleChange}
             />
-            <Button variant="outline-success"
-              className="me-3" onClick={handleSubmit}>Aceptar</Button>
-            <Button variant="outline-success"
-              className="me-3" onClick={()=>setShowTopic(false)}>Cancelar</Button>
+            <Button
+              variant="outline-success"
+              className="me-3"
+              onClick={handleSubmit}
+            >
+              Aceptar
+            </Button>
+            <Button
+              variant="outline-success"
+              className="me-3"
+              onClick={() => setShowTopic(false)}
+            >
+              Cancelar
+            </Button>
           </Form.Group>
         </Form>
       </Col>
     </Row>
-  )
-}
+  );
+};
--- server/controllers/coursesControllers.js
+++ server/controllers/coursesControllers.js
@@ -1,31 +1,36 @@
 const connection = require("../config/db");
 
-
 class coursesControllers {
   createCourse = (req, res) => {
-    const tags = JSON.parse(req.body.tags)
-    const { title, description, price, user_id } = JSON.parse(req.body.crearCurso);
+    const tags = JSON.parse(req.body.tags);
+    const { title, description, price, user_id } = JSON.parse(
+      req.body.crearCurso
+    );
 
-    let sql = `INSERT INTO course (title, description, price, user_id, img) VALUES ('${title}', '${description}', ${price}, ${user_id}, 'default.jpg')`
-    if(req.file !== undefined){
-     let img = req.file.filename;
-      sql = `INSERT INTO course (title, description, price, user_id, img) VALUES ('${title}', '${description}', ${price}, ${user_id}, '${img}')`
+    let sql = `INSERT INTO course (title, description, price, user_id, img) VALUES ('${title}', '${description}', ${price}, ${user_id}, 'default.jpg')`;
+    if (req.file !== undefined) {
+      let img = req.file.filename;
+      sql = `INSERT INTO course (title, description, price, user_id, img) VALUES ('${title}', '${description}', ${price}, ${user_id}, '${img}')`;
     }
-    connection.query(sql, (err, result)=>{
-      err && res.status(500).json(err)
-      if(err){console.log(err)}
-      let course_id = result.insertId
+    connection.query(sql, (err, result) => {
+      err && res.status(500).json(err);
+      if (err) {
+        console.log(err);
+      }
+      let course_id = result.insertId;
 
-      tags.forEach((elem) =>{
-        let sql2 = `INSERT INTO course_tag (course_id, tag_id) VALUES (${course_id}, ${elem.value})`
-        connection.query(sql2, (errtag, restag)=>{
-          errtag && res.status(500).json(errtag)
-          if(errtag){console.log(errtag)}          
-        })
-      })
-      res.status(200).json(result)
-    })
-  }
+      tags.forEach((elem) => {
+        let sql2 = `INSERT INTO course_tag (course_id, tag_id) VALUES (${course_id}, ${elem.value})`;
+        connection.query(sql2, (errtag, restag) => {
+          errtag && res.status(500).json(errtag);
+          if (errtag) {
+            console.log(errtag);
+          }
+        });
+      });
+      res.status(200).json(result);
+    });
+  };
 
   callTags = (req, res) => {
     let sql = `SELECT * FROM tag`;
@@ -38,107 +43,111 @@ class coursesControllers {
     });
   };
 
-  callCourses = (req, res) =>{
-    let sql = `SELECT * FROM course WHERE is_disabled = 1 AND is_deleted = 0`
+  callCourses = (req, res) => {
+    let sql = `SELECT * FROM course WHERE is_disabled = 1 AND is_deleted = 0`;
     // TODO is disabled = 0
-    connection.query(sql, (err, result)=>{
-      if(err){
-        res.status(500).json(err)
-      }else{
-        res.status(200).json(result)
+    connection.query(sql, (err, result) => {
+      if (err) {
+        res.status(500).json(err);
+      } else {
+        res.status(200).json(result);
       }
-    })
-  }
+    });
+  };
 
   purchaseCourse = (req, res) => {
-
     const { id } = req.params;
     let sql = `UPDATE course SET is_completed = 1 WHERE course_id = ${id} AND is_deleted = 0`;
     //TODO: cambiare is_completed con is_bought
-    connection.query(sql, (err, result)=>{
-      if(err){
-        res.status(500).json(err)
-      }else{
-        res.status(200).json(result)
-    }}
-  )}
-    
+    connection.query(sql, (err, result) => {
+      if (err) {
+        res.status(500).json(err);
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  };
 
   oneCourse = (req, res) => {
     const { course_id } = req.params;
     // console.log(course_id);
-    let sql = `SELECT course.title, course.img, course.date, course.is_completed, course.description, course.price , section.section_id, section.section_title FROM course LEFT JOIN  section ON course.course_id = section.course_id WHERE course.course_id = ${course_id} AND is_deleted = 0` ;
+    let sql = `SELECT course.title, course.img, course.date, course.is_completed, course.description, course.price , section.section_id, section.section_title FROM course LEFT JOIN  section ON course.course_id = section.course_id WHERE course.course_id = ${course_id} AND is_deleted = 0`;
     connection.query(sql, (err, result) => {
       if (err) {
         res.status(500).json(err);
       }
       //console.log(result);
-      const {title,description, img, date, price, is_completed} = result[0];
-      
+      const { title, description, img, date, price, is_completed } = result[0];
+
       let data = {
         title,
         img,
         date,
         price,
         description,
-        sections:[]
-      }
-      result.forEach(elem=>{
-        if(elem.section_id != null){
-          data.sections.push({section_id: elem.section_id, section_title: elem.section_title})
+        sections: [],
+      };
+      result.forEach((elem) => {
+        if (elem.section_id != null) {
+          data.sections.push({
+            section_id: elem.section_id,
+            section_title: elem.section_title,
+          });
         }
-      })
-      /* console.log(data); */
-      res.status(200).json(data)
-
       });
-  }
-  
+      /* console.log(data); */
+      res.status(200).json(data);
+    });
+  };
+
   editOneCourse = (req, res) => {
-    const { title, description, price, course_id, user_id } = JSON.parse(req.body.editarCurso);
+    const { title, description, price, course_id, user_id } = JSON.parse(
+      req.body.editarCurso
+    );
     let sql = `UPDATE course SET title = '${title}', description = '${description}', price = ${price} WHERE course_id = ${course_id} AND user_id = ${user_id} AND is_deleted = 0`;
-    
-    let img
+
+    let img;
     if (req.file) {
-      img = req.file.filename
-      sql = `UPDATE course SET title = '${title}', description = '${description}', price = ${price}, img = '${img}' WHERE course_id = ${course_id} AND user_id = ${user_id} AND is_deleted = 0`
+      img = req.file.filename;
+      sql = `UPDATE course SET title = '${title}', description = '${description}', price = ${price}, img = '${img}' WHERE course_id = ${course_id} AND user_id = ${user_id} AND is_deleted = 0`;
     }
     connection.query(sql, (err, result) => {
       if (err) {
         console.log("Error en la consulta SQL:", err);
         return res.status(500).json(err);
       }
-      res.status(200).json({ result, img: req.file ? req.file.filename : null });
+      res
+        .status(200)
+        .json({ result, img: req.file ? req.file.filename : null });
     });
-  }
+  };
 
   addSection = (req, res) => {
-    const {course_id, newSection} = req.body;
+    const { course_id, newSection } = req.body;
 
     let sql_cont = `SELECT max(section_id) as id FROM section WHERE course_id = ${course_id}`;
 
-    connection.query(sql_cont,(err1,result)=>{
-      if(err1){
-        return res.status(500).json(err1)
+    connection.query(sql_cont, (err1, result) => {
+      if (err1) {
+        return res.status(500).json(err1);
       }
-     
-     let section_id = result[0].id ;
-     if(section_id == null){
-      section_id = 1
-     }else{
-      section_id ++;
-     }
-     //console.log(section_id);
-     let sql_insert = `INSERT INTO section (course_id, section_id, section_title) VALUES (${course_id} , ${section_id} , "${newSection}")`
 
-     connection.query(sql_insert, (err2, result_insert )=>{
-        if(err2){
-          return res.status(500).json(err2)
+      let section_id = result[0].id;
+      if (section_id == null) {
+        section_id = 1;
+      } else {
+        section_id++;
+      }
+      //console.log(section_id);
+      let sql_insert = `INSERT INTO section (course_id, section_id, section_title) VALUES (${course_id} , ${section_id} , "${newSection}")`;
+
+      connection.query(sql_insert, (err2, result_insert) => {
+        if (err2) {
+          return res.status(500).json(err2);
         }
-        res.status(201).json({result_insert, section_id})
-
-     })
-    })
+        res.status(201).json({ result_insert, section_id });
+      });
+    });
 
     /* let sql = `INSERT INTO section (course_id, section_id, section_title) VALUES ('${course_id}', '${section_id}', '${section_title}')` */
 
@@ -149,77 +158,55 @@ class coursesControllers {
         res.status(200).json(result)
       }
     }) */
-  }
+  };
 
   viewPurchasedCourse = (req, res) => {
-    let sql = `SELECT * FROM course WHERE is_completed = 1 AND is_deleted = 0`
+    let sql = `SELECT * FROM course WHERE is_completed = 1 AND is_deleted = 0`;
     //TODO: cambiare is_completed con is_bought`
     connection.query(sql, (err, result) => {
-      err ?
-      res.status(500).json(err)
-      :
-      res.status(200).json(result)
-    })
-  }
+      err ? res.status(500).json(err) : res.status(200).json(result);
+    });
+  };
 
   //este controlador es para los cursos guardados como favoritos
   viewSavedCourse = (req, res) => {
-    let sql = `SELECT * FROM course WHERE IS_LIKED = 1 AND is_deleted = 0`
+    let sql = `SELECT * FROM course WHERE IS_LIKED = 1 AND is_deleted = 0`;
     connection.query(sql, (err, result) => {
-      err ?
-      res.status(500).json(err)
-      :
-      res.status(200).json(result)
-    })
-  }
+      err ? res.status(500).json(err) : res.status(200).json(result);
+    });
+  };
 
   deleteSection = (req, res) => {
-    const {course_id, section_id} = req.params;
+    const { course_id, section_id } = req.params;
     console.log("PARAMS", req.params);
-    let sql = `DELETE FROM section WHERE course_id = ${course_id} and section_id =${section_id}`
+    let sql = `DELETE FROM section WHERE course_id = ${course_id} and section_id =${section_id}`;
 
-    connection.query(sql, (err, result) =>{
-      err ?
-      res.status(500).json(err)
-      :
-      res.status(200).json(result)
-    })
-  }
+    connection.query(sql, (err, result) => {
+      err ? res.status(500).json(err) : res.status(200).json(result);
+    });
+  };
 
-  addTopic = (req, res) =>{
-    const {course_id, newTopic, section_id} = req.body
-
-    console.log(req.body)
-
+  addTopic = (req, res) => {
+    const { course_id, newTopic, section_id } = req.body;
     let sql_cont = `SELECT max(topic_id) as id FROM topic WHERE section_id = ${section_id}`;
-
-    connection.query(sql_cont, (err, result)=>{
-      if(err){
-        return res.status(500).json(err)
+    connection.query(sql_cont, (err, result) => {
+      if (err) {
+        return res.status(500).json(err);
       }
-     
-      let topic_id = result[0].id ;
-      if(topic_id == null){
-      topic_id = 1
-      }else{
-      topic_id ++;
+      let topic_id = result[0].id;
+      if (topic_id == null) {
+        topic_id = 1;
+      } else {
+        topic_id++;
       }
-
-      let sql_insert = `INSERT INTO topic (course_id, section_id topic_id, topic_title) VALUES (${course_id}, ${section_id}, ${topic_id}, "${newTopic}")`
-
-        connection.query(sql_insert, (err2, result_insert )=>{
-        if(err2){
-          return res.status(500).json(err2)
+      let sql_insert = `INSERT INTO topic (course_id, section_id, topic_id, topic_title) VALUES (${course_id}, ${section_id}, ${topic_id}, "${newTopic}")`;
+      connection.query(sql_insert, (err2, result_insert) => {
+        if (err2) {
+          return res.status(500).json(err2);
         }
-        res.status(201).json({result_insert, topic_id})
-
-     })
-
-    })
-
-  }
-
-
+        res.status(201).json({ result_insert, topic_id });
+      });
+    });
+  };
 }
 module.exports = new coursesControllers();
-
