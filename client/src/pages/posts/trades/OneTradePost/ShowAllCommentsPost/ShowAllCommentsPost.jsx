0c9e4a1e01c5ab52ac147cfa4d18ee21f2b8a5f9
Cristian <cristian.mlg.94@hotmail.com>
2024-01-24 16:56:25 +0100

    modificaciones varias

--- client/src/pages/posts/trades/OneTradePost/ShowAllCommentsPost/ShowAllCommentsPost.jsx
+++ client/src/pages/posts/trades/OneTradePost/ShowAllCommentsPost/ShowAllCommentsPost.jsx
@@ -1,10 +1,15 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { AscendioContext } from "../../../../../context/AscendioContext";
 
 export const ShowAllCommentsPost = ({ showModal, setShowModal, oneTrade }) => {
   const [showComments, setShowComments] = useState();
+  const [update, setUpade] = useState(false);
 
   const { post_id } = oneTrade;
+
+  const { user } = useContext(AscendioContext);
 
   if (post_id) {
     useEffect(() => {
@@ -13,25 +18,51 @@ export const ShowAllCommentsPost = ({ showModal, setShowModal, oneTrade }) => {
         .then((res) => {
           console.log(res);
           setShowComments(res.data.result);
+          setUpade(false);
         })
         .catch((error) => console.log(error));
-    }, []);
+    }, [update]);
   }
+
+  const deleteComment = (comment_id) => {
+    axios
+      .put(`http://localhost:3000/comments/deletecomments/${comment_id}`)
+      .then((res) => {
+        console.log(res);
+        setUpade(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  console.log(showComments);
 
   return (
     <div>
       <h3>Comentarios del Post</h3>
-      <div>
-        {showComments?.map((elem) => {
-          return (
-            <div key={elem.comment_id} className="d-flex justify-content-center gap-5">
-              <h6 className="col-3">{elem.nickname}</h6>
-              <p className="col-6">{elem.message}</p>
-              <h6 className="col-3">{elem.date.slice(11,16)} / {elem.date.slice(0, 10).split("-").reverse().join("-")}</h6>
-            </div>
-          );
-        })}
-      </div>
+      <hr />
+      {showComments?.map((elem) => {
+        return (
+          <div key={elem.comment_id} className="d-flex gap-5 mb-1">
+            <h6 className="col-1">{elem.nickname}</h6>
+            <p className="col-7">{elem.message}</p>
+            <h6 className="col-2">
+              {elem.date.slice(11, 16)} /{" "}
+              {elem.date.slice(0, 10).split("-").reverse().join("-")}
+            </h6>
+            {user.user_id === elem.user_id && (
+              <Button
+                className="col-1"
+                onClick={() => {
+                  deleteComment(elem.comment_id);
+                }}
+              >
+                Eliminar
+              </Button>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
