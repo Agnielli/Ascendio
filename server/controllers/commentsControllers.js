542cb5d5d3ffa335ac81e9dc9acb309440774b77
Cristian <cristian.mlg.94@hotmail.com>
2024-01-24 19:08:39 +0100

    hola

--- server/controllers/commentsControllers.js
+++ server/controllers/commentsControllers.js
@@ -4,33 +4,45 @@ require("dotenv").config();
 class CommentsControllers {
   createComment = (req, res) => {
     try {
-      console.log(req.body);
-
       const comentario = req.body.comment.message;
       const post_id = req.body.oneTrade.post_id;
       const user_id = req.body.user.user_id;
 
-      let sql = `SELECT COUNT(*) AS num_comentarios
-      FROM comments
-      WHERE post_id = ${post_id};`;
-
+      let sql = `SELECT MAX(comment_id) AS num_comentarios FROM comments WHERE post_id = ${post_id};`;
       connection.query(sql, (error, result) => {
         if (error) {
           console.log(error);
           res.status(400).json({ message: "Error en la SQL" });
         } else {
-          let numComentarios = parseInt(result[0].num_comentarios) + 1;
-          let sql2 = `INSERT INTO comments (comment_id, user_id, post_id, date, message) VALUES (${numComentarios}, ${user_id}, ${post_id}, NOW(), '${comentario}');`;
-          connection.query(sql2, (error, result) => {
-            if (error) {
-              console.log(error);
-              res.status(400).json({ message: "Error en la SQL2" });
-            } else {
-              res
-                .status(200)
-                .json({ message: "Comentario creado", result: result });
-            }
-          });
+          if (result[0].num_comentarios === null) {
+            let numComentarios = 1;
+            console.log("caso1", numComentarios);
+            let sql2 = `INSERT INTO comments (comment_id, user_id, post_id, date, message) VALUES (${numComentarios}, ${user_id}, ${post_id}, NOW(), '${comentario}');`;
+            connection.query(sql2, (error, result) => {
+              if (error) {
+                console.log(error);
+                res.status(400).json({ message: "Error en la SQL2" });
+              } else {
+                res
+                  .status(200)
+                  .json({ message: "Comentario creado", result: result });
+              }
+            });
+          } else {
+            let numComentarios = parseInt(result[0].num_comentarios) + 1;
+            console.log("caso2", numComentarios);
+            let sql2 = `INSERT INTO comments (comment_id, user_id, post_id, date, message) VALUES (${numComentarios}, ${user_id}, ${post_id}, NOW(), '${comentario}');`;
+            connection.query(sql2, (error, result) => {
+              if (error) {
+                console.log(error);
+                res.status(400).json({ message: "Error en la SQL2" });
+              } else {
+                res
+                  .status(200)
+                  .json({ message: "Comentario creado", result: result });
+              }
+            });
+          }
         }
       });
     } catch (error) {
