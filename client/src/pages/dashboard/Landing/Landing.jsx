7b79441df6e475abc3e747f8c23d94f1938c5aa4
juanjo <juanjopalma03@gmail.com>
2024-01-25 15:38:40 +0100

    grafica en landing

--- client/src/pages/dashboard/Landing/Landing.jsx
+++ client/src/pages/dashboard/Landing/Landing.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { TradingViewWidget } from '../../../components/TradingViewWidget/TradingViewWidget'
 
 export const Landing = () => {
     return (
     <>
     <h2 >Landing</h2>
-    
+    <TradingViewWidget />
     <footer className="d-flex justify-content-between align-items-center ">
       <h3>ASCENDIO</h3>
       <p> &copy; 2024 Ascendio, inc</p>
