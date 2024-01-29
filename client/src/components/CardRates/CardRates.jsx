98c13c540141a4c23024afd00b0e271d3fc536ce
federico <fgiannini95@gmail.com>
2024-01-29 16:18:56 +0100

    arreglo

--- client/src/components/CardRates/CardRates.jsx
+++ client/src/components/CardRates/CardRates.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Card, Button, Form } from "react-bootstrap";
 import { AscendioContext } from "../../context/AscendioContext";
 import { useParams } from "react-router-dom";
