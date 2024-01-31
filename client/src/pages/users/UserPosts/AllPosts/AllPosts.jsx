9ec5837745e8fb7601bef78929a30d08ed7b33b8
Cristian <cristian.mlg.94@hotmail.com>
2024-01-31 18:45:06 +0100

    retocandoLOTODO!

--- client/src/pages/users/UserPosts/AllPosts/AllPosts.jsx
+++ client/src/pages/users/UserPosts/AllPosts/AllPosts.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Button, Card, ListGroup } from "react-bootstrap";
+import { Button, Card, Col, ListGroup, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import "./AllPosts.scss"
-import "../../../../../../client/public/stylesheets/ButtonsApp.scss"
-
+import "./AllPosts.scss";
+import "../../../../../../client/public/stylesheets/ButtonsApp.scss";
 
 export const AllPosts = ({
   posts,
@@ -13,44 +12,43 @@ export const AllPosts = ({
 }) => {
   const navigate = useNavigate();
   return (
-    <div className=" AllPostsScss d-flex flex-wrap">
-      {posts?.map((elem) => {
-        return (
-          <Card className="ESTILOCARD" key={elem.post_id}>
-            {elem.resource_text !== null ? (
-              <Card.Img
-                variant="top"
-                src={`http://localhost:3000/images/trades/${elem.resource_text}`}
-              />
-            ) : <Card.Img
-            
-            variant="top"
-            src={"../../../../../public/images/trade/trades.png"}
-          />}
-            <Card.Body>
-              {/* <Card.Title>Categoría: {elem.category_name}</Card.Title> */}
-              {elem.currency !== null ? (
-                <ListGroup variant="flush">
-                  Detalles:
-                  <ListGroup.Item>Categoría: {elem.category_name}</ListGroup.Item>
-
+    <Row className=" AllPostsScss d-flex flex-wrap">
+      <Col>
+        {posts?.map((elem) => {
+          return (
+            <Card key={elem.post_id}>
+              {elem.resource_text === null ? (
+                <Card.Img
+                  variant="top"
+                  src={`http://localhost:3000/images/trades/LOQUESEA`}
+                />
+              ) : (
+                <Card.Img
+                  variant="top"
+                  src={
+                    elem.type === 2
+                      ? `http://localhost:3000/images/trades/${elem.resource_text}`
+                      : `http://localhost:3000/images/generalpost/${elem.resource_text}`
+                  }
+                />
+              )}
+              {elem.type === 2 ? (
+                <Card.Body>
+                  <Card.Title> {elem.category_name} </Card.Title>
                   <Card.Text>
-                  Descripción: {elem.description}
-                  <br/>
-                  Currency: {elem.currency}
-                  <br/>
-                  Precio de entrada: {elem.entry_price}
-                  <br/>
-                  Precio de stop: {elem.stop_loss}
-                  <br/>
-                  Precio Profit: {elem.take_profit}
-                  <br/>
-                  <br/>
-                    Estado: {elem.correct === null && "trade pendiente"}
-                    {elem.correct === 0 && "trade errado"}
-                    {elem.correct === 1 && "trade Acertado"}
+                    {elem.currency !== null ? (
+                      <p>Currency: {elem.currency}</p>
+                    ) : null}
+                    <p>Descripción: {elem.description}</p>
+                    <p>Precio de entrada: {elem.entry_price}</p>
+                    <p>Precio de stop: {elem.stop_loss}</p>
+                    <p>Precio Profit: {elem.take_profit}</p>
+                    <p>
+                      Estado: {elem.correct === null && "trade pendiente"}
+                      {elem.correct === 0 && "trade errado"}
+                      {elem.correct === 1 && "trade Acertado"}
+                    </p>
                   </Card.Text>
-                  
                   <div className="d-flex justify-content-center mt-1 mb-1">
                     <Button
                       onClick={() => {
@@ -59,94 +57,94 @@ export const AllPosts = ({
                     >
                       Ir a comentarios del post
                     </Button>
-                  </div>
-                  {elem.correct === null ? (
-                    <div className="d-flex gap-1">
-                      <Button
-                        onClick={() => markACorrect(elem.post_id, elem.correct)}
-                        variant="primary"
-                      >
-                        Marcar Acertado
-                      </Button>
-                      <Button
-                        onClick={() =>
-                          markAIncorrect(elem.post_id, elem.correct)
-                        }
-                        variant="primary"
-                      >
-                        Marcar Errado
-                      </Button>
-                    </div>
-                  ) : elem.correct === 1 ? (
-                    <div className="d-flex gap-1">
-                      <Button
-                        onClick={() =>
-                          markAIncorrect(elem.post_id, elem.correct)
-                        }
-                        variant="primary"
-                      >
-                        Marcar Errado
-                      </Button>
-                      <Button
-                        onClick={() => markAPending(elem.post_id, elem.correct)}
-                        variant="primary"
-                      >
-                        Marcar Pendiente
-                      </Button>
-                    </div>
-                  ) : (
-                    <>
-                      <div>
-                        <div className="d-flex gap-1 mb-1">
-                          <Button
-                          className="Button2"
-                            onClick={() =>
-                              markACorrect(elem.post_id, elem.correct)
-                            }
-                            variant="primary"
-                          >
-                            Marcar Acertado
-                          </Button>
-                          <Button
-                          className="Button2"
-                            onClick={() =>
-                              markAPending(elem.post_id, elem.correct)
-                            }
-                            variant="primary"
-                          >
-                            Marcar Pendiente
-                          </Button>
-                        </div>
+                    {elem.correct === null ? (
+                      <div className="d-flex gap-1">
+                        <Button
+                          onClick={() =>
+                            markACorrect(elem.post_id, elem.correct)
+                          }
+                          variant="primary"
+                        >
+                          Marcar Acertado
+                        </Button>
+                        <Button
+                          onClick={() =>
+                            markAIncorrect(elem.post_id, elem.correct)
+                          }
+                          variant="primary"
+                        >
+                          Marcar Errado
+                        </Button>
                       </div>
-                    </>
-                  )}
-                </ListGroup>
+                    ) : elem.correct === 1 ? (
+                      <div className="d-flex gap-1">
+                        <Button
+                          onClick={() =>
+                            markAIncorrect(elem.post_id, elem.correct)
+                          }
+                          variant="primary"
+                        >
+                          Marcar Errado
+                        </Button>
+                        <Button
+                          onClick={() =>
+                            markAPending(elem.post_id, elem.correct)
+                          }
+                          variant="primary"
+                        >
+                          Marcar Pendiente
+                        </Button>
+                      </div>
+                    ) : (
+                      <>
+                        <div>
+                          <div className="d-flex gap-1 mb-1">
+                            <Button
+                              className="Button2"
+                              onClick={() =>
+                                markACorrect(elem.post_id, elem.correct)
+                              }
+                              variant="primary"
+                            >
+                              Marcar Acertado
+                            </Button>
+                            <Button
+                              className="Button2"
+                              onClick={() =>
+                                markAPending(elem.post_id, elem.correct)
+                              }
+                              variant="primary"
+                            >
+                              Marcar Pendiente
+                            </Button>
+                          </div>
+                        </div>
+                      </>
+                    )}
+                  </div>
+                </Card.Body>
               ) : (
-                <div>
-                  <Card.Img
-                  className="IMGGeneral"
-                    variant="top"
-                    src={`http://localhost:3000/images/generalPost/${elem.resource_text}`}
-                  />
-                  <ListGroup.Item><p>Categoría: {elem.category_name}</p></ListGroup.Item>
-                  
-                    <Card.Text className="d-flex flex-column">
-                      <p>Descripción: {elem.description}</p>
-                    </Card.Text>
-                  
+                <Card.Body>
+                  <Card.Title> {elem.category_name} </Card.Title>
+                  <Card.Text>
+                    {elem.currency !== null ? (
+                      <p>Currency: {elem.currency}</p>
+                    ) : null}
+                    <p>Descripción: {elem.description}</p>
+                  </Card.Text>
                   <Button
                     onClick={() => {
-                      navigate(`/oneGeneralPost/${elem.post_id}`);
+                      navigate(`/onegeneralpost/${elem.post_id}`);
                     }}
                   >
                     Ir a comentarios del post
                   </Button>
-                </div>
+                </Card.Body>
               )}
-            </Card.Body>
-          </Card>
-        );
-      })}
-    </div>
+            </Card>
+          );
+        })}
+      </Col>
+    </Row>
   );
 };
