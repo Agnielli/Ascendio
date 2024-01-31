ca2da522faacfcfd94471e61b0c428bbff37cc55
juanjo <juanjopalma03@gmail.com>
2024-01-31 14:05:35 +0100

    mis posts in process

--- client/src/pages/users/UserPosts/UserPosts.jsx
+++ client/src/pages/users/UserPosts/UserPosts.jsx
@@ -6,6 +6,7 @@ import { Button, Card, ListGroup } from "react-bootstrap";
 import { AllPosts } from "./AllPosts/AllPosts";
 import { AllGeneralPosts } from "./AllGeneralPosts/AllGeneralPosts";
 import { AllTradePosts } from "./AllTradePosts/AllTradePosts";
+import './userPosts.scss';
 
 export const UserPosts = () => {
   const [posts, setPosts] = useState([]);
@@ -79,13 +80,19 @@ export const UserPosts = () => {
   };
 
   return (
-    <>
+    <div className="userPosts">
       <div className="d-flex p-5 gap-5">
         <h2>Mis Posts</h2>
-        <Button onClick={() => navigate("/profile")}>Volver</Button>
+        <Button 
+          className="Button2"
+          onClick={() => navigate("/profile")}
+        >
+          Volver
+        </Button>
       </div>
-      <div className="d-flex gap-5 ps-5 pe-5 mb-1">
+      <div className="d-flex justify-content-between stylesbuttons">
         <Button
+          className="Button2"
           variant={showFilter === 0 ? "danger" : "primary"}
           onClick={() => {
             setShowFilter(0);
@@ -94,12 +101,14 @@ export const UserPosts = () => {
           Todos
         </Button>
         <Button
+          className="Button2"
           variant={showFilter === 2 ? "danger" : "primary"}
           onClick={() => setShowFilter(2)}
         >
           Trades
         </Button>
         <Button
+          className="Button2"
           variant={showFilter === 1 ? "danger" : "primary"}
           onClick={() => setShowFilter(1)}
         >
@@ -130,6 +139,6 @@ export const UserPosts = () => {
           markAPending={markAPending}
         />
       )}
-    </>
+    </div>
   );
 };
