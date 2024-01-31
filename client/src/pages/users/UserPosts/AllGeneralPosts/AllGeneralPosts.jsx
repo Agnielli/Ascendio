522da20d288c821705c41b386b095f9821d65842
carlos <carlosih1209@gmail.com>
2024-01-31 18:47:25 +0100

    carlos

--- client/src/pages/users/UserPosts/AllGeneralPosts/AllGeneralPosts.jsx
+++ client/src/pages/users/UserPosts/AllGeneralPosts/AllGeneralPosts.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button, Card, ListGroup } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./AllGeneralPosts.scss";
 
 export const AllGeneralPosts = ({
   posts,
@@ -7,22 +9,34 @@ export const AllGeneralPosts = ({
   markAIncorrect,
   markAPending,
 }) => {
+  const navigate = useNavigate();
   return (
-    <div className="d-flex gap-5 flex-wrap pe-5 ps-5">
+    <div className="ALLGeneralPostsScss d-flex flex-wrap ">
       {posts
         ?.filter((post) => post.type === 1)
         .map((elem) => {
           return (
-            <Card style={{ width: "18rem" }} key={elem.post_id}>
+            <Card className="ESTILOCARD" key={elem.post_id}>
               {elem.resource_text !== null ? (
                 <Card.Img
                   variant="top"
                   src={`http://localhost:3000/images/generalPost/${elem.resource_text}`}
                 />
-              ) : null}
+              ) : <Card.Img
+            
+              variant="top"
+              src={"../../../../../public/images/trade/trades.png"}
+            />}
               <Card.Body>
                 <Card.Title>Categoría: {elem.category_name}</Card.Title>
                 <Card.Text>Descripción: {elem.description}</Card.Text>
+                <Button
+                    onClick={() => {
+                      navigate(`/oneGeneralPost/${elem.post_id}`);
+                    }}
+                  >
+                    Ir a comentarios del post
+                  </Button>
               </Card.Body>
             </Card>
           );
